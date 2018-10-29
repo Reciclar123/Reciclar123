cbccd4651f40a3f90acdd1a853cccb12b3fd5583
JorgeEduardoH <jehernandezrodriguez@gmail.com>
2018-10-29 11:25:31 -0500

    api-rest cambiado login para retornar datos de usuario y registro en un solo modelo

--- common/models/person.js
+++ common/models/person.js
@@ -1,5 +1,42 @@
 'use strict';
+var app = require('../../server/server');
+
 
 module.exports = function(Person) {
 
+Person.validatesUniquenessOf('telefono','email','username');
+
+
+  Person.afterRemote('login', function(ctx, modelInstance, next) {
+var person = {};
+
+
+    if (ctx.result)
+    {
+
+    Person.findById(ctx.result.userId,(error,p)=>{
+
+      if (!error ) {
+
+        ctx.result.nombre=  p.nombre;
+          ctx.result.rol=  p.rol;
+            ctx.result.cedula=  p.cedula;
+              ctx.result.gotas=  p.gotas;
+                ctx.result.telefono=  p.telefono;
+                ctx.result.email=  p.email;
+                ctx.result.ciudad=  p.ciudad;
+      }
+
+
+      next();
+
+    });
+
+
+
+
+}else
+ next();
+
+  });
 };
