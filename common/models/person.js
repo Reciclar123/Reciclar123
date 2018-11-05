23f652df9a026d69918f56e1923eea0bebbdb50b
JorgeEduardoH <jehernandezrodriguez@gmail.com>
2018-11-04 21:39:43 -0500

    creacion de servicio de recuperar contraseña

--- common/models/person.js
+++ common/models/person.js
@@ -1,10 +1,60 @@
 'use strict';
 var app = require('../../server/server');
-
+var generator = require('generate-password');
 
 module.exports = function(Person) {
 
-  //Person.validatesUniquenessOf('telefono','email','username');
+  Person.validatesUniquenessOf('telefono','email','username');
+
+  Person.recoveryPassword = function (email,cb) {
+
+
+  Person.findOne({ where: {email: email} }, function(err, user) {
+    if (err) {
+      cb(err);
+    } else if (user) {
+
+      var password = generator.generate({length: 10,numbers: true});
+      user.password = Person.hashPassword(password);
+
+        Person.upsert(user,(err,res)=>{
+
+          if (err) {
+            var errorm = new Error("No se encuentra la persona");
+            errorm.status = 401;
+            cb(errorm);
+          }else {
+
+            Person.app.models.Email.send({
+              to: email,
+              from: 'you@gmail.com',
+              subject: 'Cambio de password',
+              text: 'my text',
+              html: 'tu nueva contraseña <br><br>'+password+' <em>por favor ten cuidado con tus datos de acceso</em>'
+            }, function(err, mail) {
+              console.log('email sent!');});
+              cb(null,{msg:'se actualizo correctamente,revisa el correo.'});
+          }
+      });
+
+    }else {
+      var errorm = new Error("No se encuentra la persona");
+      errorm.status = 401;
+      cb(errorm);
+
+    }
+});
+  }
+
+  Person.remoteMethod(
+'recoveryPassword',
+{
+  accepts: [
+    {arg: 'Email', type: 'string', required: true}
+  ],
+  returns: [{arg: 'response', type: 'array'}],
+}
+);
 
   Person.afterRemote('login', function(ctx, modelInstance, next) {
     if (ctx.result)
@@ -40,23 +90,22 @@ module.exports = function(Person) {
 
  Person.beforeRemote( 'create', function( ctx, modelInstance, next) {
     ctx.req.body.pass = ctx.req.body.password;
-    ctx.req.body.addressP = ctx.req.body.address;
-   //ctx.req.body.unsetAttribute('address');
-    next();
+
+    if (!ctx.req.body.address) {
+      var err = new Error("No se puede guardar sin la direccion");
+      err.statusCode = 400;
+      console.log(err.toString());
+      next(err);
+    }else {
+        ctx.req.body.addressP = ctx.req.body.address;
+        
+        next();
+    }
 });
 
 Person.observe('after save', function(ctx, next) {
 
    if (ctx.isNewInstance !== undefined) { //new user
-
-     /*Person.app.models.Email.send({
-      to: ctx.instance.email,
-      from: 'greentyapp@gmail.com',
-      subject: 'Registro',
-      text: 'Gracias por tu registro',
-      html: 'my <em>Bienvenido a greenty(ciudad verde ) </em>'
-    });*/
-
 
       Person.login({"username" : ctx.instance.username,"password" : ctx.instance.pass}, (loginErr, loginToken)=>
      {
@@ -76,34 +125,19 @@ Person.observe('after save', function(ctx, next) {
                  days : ctx.instance.addressP.days
              },(e,addr)=>{
                      if(!e){
-
                         ctx.instance.addressPerson = addr ;
-
                      }else {
                          ctx.instance.addressPerson = "error";
                      }
-
                      ctx.instance.unsetAttribute('addressP');
-
                     next();
-
-
-
              });
-
-
             }
-
-
-
          }else {
 
            next();
          }
      });
-
-
-
 
    }else {
       next();
@@ -111,47 +145,6 @@ Person.observe('after save', function(ctx, next) {
 
 });
 
-/* Person.observe('after save', async (ctx, next)=> {
 
-    if (ctx.isNewInstance !== undefined) { //new user
-      let addressnew = null;
-      if (ctx.instance.addressPerson !== null) {
-          let    addressPerson = app.models.addressPerson;
-           addressnew = await addressPerson.create({
-              personId : ctx.instance.id,
-              address : ctx.instance.addressPerson.address,
-              days : ctx.instance.addressPerson.days
-          },(e,addr)=>{ if(!e)console.log(addr);});
 
-          if(addressnew)
-          {
-             let address = [];
-             address.push(addressnew);
-             console.log(address);
-             ctx.instance.updateAttributes({address: address});
-          }
-          //ctx.instance.unsetAttribute('addressPerson');
-      }
-
-     // create login
-     let login = await Person.login({"username" : ctx.instance.username,"password" : ctx.instance.pass}, (loginErr, loginToken)=>
-      {
-
-          if (loginToken) {
-             ctx.instance.personId = loginToken.personId;
-             ctx.instance.ttl = loginToken.ttl;
-             ctx.instance.tokenId = loginToken.id;
-             next();
-          }else {
-            next();
-          }
-      });
-
-    }else {
-       next();
-    }
-
-});
-
-*/
 };
